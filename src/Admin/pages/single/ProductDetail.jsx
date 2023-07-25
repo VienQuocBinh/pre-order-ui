78642bf341fa5a87a820501461fa5d24d95237d5
tuanlac <peanuttfpg@gmail.com>
2023-07-26 02:14:52 +0700

    add Firebase image upload to product

--- src/Admin/pages/single/ProductDetail.jsx
+++ src/Admin/pages/single/ProductDetail.jsx
@@ -8,13 +8,38 @@ import { Navbar } from "../../components/navbar/Navbar";
 import { Chart } from "../../components/chart/Chart";
 import { List } from "../../components/table/Table";
 import { CircularProgress } from "@mui/material";
+import { ref  
+        ,uploadBytes
+        ,getDownloadURL } from "firebase/storage";
+import {storage} from "../../firebase/firebase";
+import { useToast } from "@chakra-ui/react";
 
 export const ProductDetail = () => {
+  const toast = useToast();
   const param = useParams();
   const [product, setProduct] = useState(Product);
   const { getById } = useProductContext();
   const [imgUrl, setImgUrl] = useState("");
+  const [imageUpload, setImageUpload] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const UploadImage = () => {
+    if(imageUpload == null) return;
+    
+    const imageRef = ref(storage, `images/${imageUpload.name}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot)=>{
+      getDownloadURL(snapshot.ref).then((downloadURL) => {
+        setImgUrl(downloadURL);
+      });
+      toast({
+        title: "Đăng hình ảnh thành công!",
+          status: "success",
+          position: "top-right",
+          isClosable: true,
+          duration: 1000,
+      })
+    });
+  }
 
   useEffect(() => {
     setLoading(true);
@@ -27,6 +52,15 @@ export const ProductDetail = () => {
       .then(() => setLoading(false))
       .catch((err) => console.log(err));
   }, [param.productId, getById]);
+
+  useEffect(() => {
+    if(imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name}`);
+    getDownloadURL(imageRef).then((downloadURL) => {
+      setImgUrl(downloadURL);
+    });
+  },[]);
+  
 
   // console.log({ imgUrl });
 
@@ -53,7 +87,18 @@ export const ProductDetail = () => {
                 <div className="editButton">Edit</div>
                 <h1 className="title">Information</h1>
                 <div className="item">
-                  <img src={imgUrl} alt="" className="itemImg" />
+                  <img src={imgUrl} alt="" className="itemImg" width={"500px"} height={"500px"}/>
+                  <input type="file" onChange={(event) => {
+                    setImageUpload(event.target.files[0]);
+                    }
+                  } />
+                  <button onClick={UploadImage}
+                          style={{border: "2px solid blue"
+                                 ,borderRadius: "10px"
+                                 ,width:"100px"
+                                 ,height:"100px"}}>
+                    Upload Image
+                    </button>
 
                   <div className="details">
                     <h1 className="itemTitle">{product.name}</h1>
