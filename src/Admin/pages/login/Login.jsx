7b573222b089790febd358216d39b148c1fda0d8
VienQuocBinh <quocbinhvien@gmail.com>
2023-07-25 20:57:40 +0700

    Login form

--- src/Admin/pages/login/Login.jsx
+++ src/Admin/pages/login/Login.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import "./login.scss";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // You can add your login logic here, such as sending the data to the server.
+    console.log("Form data:", formData);
+    navigate("/home");
+  };
+
+  return (
+    <div className="login-form-container">
+      <form className="login-form" onSubmit={handleSubmit} autoComplete="off">
+        <h2>Login</h2>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
